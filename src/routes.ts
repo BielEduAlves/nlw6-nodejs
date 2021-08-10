e00c42371b211685bf8cf62817488d6b9b5c7e36
Gabriel Alves <bieledu@gmail.com>
2021-08-10 14:30:19 -0300

    Finalizado

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,10 @@ import { ensureAdmin } from "./middlewares/ensuresAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListuserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUserController";
 
 const router = Router();
 
@@ -12,10 +16,19 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsService = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsService.handle)
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle)
+router.get("/users/", ensureAuthenticated, listUsersController.handle)
+router.get("/tags", ensureAuthenticated, listTagsController.handle)
 
 export { router };
